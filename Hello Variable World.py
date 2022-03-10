634ab75994b63590e480e543a7eafc48cf340fee
Jessica Perez <jessicaperez@jessicas-air.myfiosgateway.com>
2022-03-10 18:29:04 -0500

    updating repo

---
+++ Hello Variable World.py
@@ -0,0 +1,19 @@
+name ='Jessica'
+country = 'USA'
+age = 11
+hourly_wage = 3 
+daily_wage = hourly_wage*8
+satisfied = True
+print("Hello",name,"!")
+print("country",country)
+print("age",age)
+print(f"daily wage: {daily_wage}")
+print(f"user satisfaction:{satisfied}")
+
+name ='Jessica'
+country = 'USA'
+age = 11
+hourly_wage = 3 
+satisfied = False
+daily_wage=hourly_wage * 8
+print(name + "from" + country + "is" +str(age) + "makes" +str(hourly_wage))
---
+++ hellp world.py
@@ -0,0 +1,11 @@
+name = 'Jessica'
+country = 'USA'
+
+age = 11
+hourly_wage = 3 
+
+satisfied = False 
+
+daily_wage = hourly_wage * 8
+
+print(name + " from " + country + " is " + str(age) + " makes " + str(daily_wage) + " dollars a day ")
