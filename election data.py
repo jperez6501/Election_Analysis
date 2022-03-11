970201a7746d63f3d92325a05e767a139c55406d
jperez6501 <100246124+jperez6501@users.noreply.github.com>
2022-03-10 19:22:12 -0500

    Delete election data.py

--- election data.py
+++
@@ -1,8 +0,0 @@
-#Assing a variable for teh file to load and the path
-file_to_load = 'Resources3/election_results.csv'
-with open(file_to_load) as election_data: 
-    print(election_data)
-#open the election results and read the file. 
-#to do: perform analysis
-#Close the file
-election_data.close()
